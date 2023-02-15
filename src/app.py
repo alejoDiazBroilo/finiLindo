e145fde3f68c030ca28a3ba9d423ac4c8cbbf076
Alejo <diazbroiloalejol@gmail.com>
2023-02-15 17:53:17 -0300

    hola?

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from routes.home import Home
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:c3koV2O5uaUGyYFIskBa@containers-us-west-78.railway.app:6912/railway"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.register_blueprint(Home)
+
+
